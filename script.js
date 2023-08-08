1e16f69cf6e8914c1feca6898d05d51eb60539a3
Qomplainerz <messanyrecordings@yahoo.de>
2023-08-09 00:17:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-// Ensure that the script has write permissions in the directory where the log.txt file is located
-
-const fs = require('fs');
-
-// Function to load responses from responses.txt
-async function loadResponses() {
-    return new Promise((resolve, reject) => {
-        fs.readFile('responses.txt', 'utf8', (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                const responses = JSON.parse(data);
-                resolve(responses);
-            }
-        });
-    });
-}
-
-// Function to load jokes from jokes.txt
-async function loadJokes() {
-    return new Promise((resolve, reject) => {
-        fs.readFile('jokes.txt', 'utf8', (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                const jokes = JSON.parse(data);
-                resolve(jokes);
-            }
-        });
-    });
-}
+let conversationLog = [];
 
 // Function to process user input
 async function processUserInput(userInput) {
@@ -60,17 +30,60 @@ async function processUserInput(userInput) {
 
     // Ask user for feedback
     displayMessage("QP GPT: ", "Were you happy with the response?");
+}
 
-    // Log user input, bot response, and feedback
-    logInteraction(userInput, botResponse);
+// Function to load responses from responses.txt
+async function loadResponses() {
+    try {
+        const response = await fetch("responses.txt");
+        const text = await response.text();
+        const lines = text.split("\n");
+
+        const responses = lines.map(line => {
+            const [input, response] = line.split("|");
+            return { input, response };
+        });
+
+        return responses;
+    } catch (error) {
+        console.error("Error loading responses:", error);
+        return [];
+    }
+}
+
+// Function to load jokes from jokes.txt
+async function loadJokes() {
+    try {
+        const response = await fetch("jokes.txt");
+        const text = await response.text();
+        const jokes = text.split("\n");
+
+        return jokes;
+    } catch (error) {
+        console.error("Error loading jokes:", error);
+        return [];
+    }
+}
+
+// Function to handle user feedback and update log
+function handleFeedback(feedback) {
+    const lastMessage = conversationLog[conversationLog.length - 1];
+    if (lastMessage && lastMessage.sender === "QP GPT") {
+        lastMessage.feedback = feedback;
+    }
+    // Update log.txt (simulated)
+    console.log(conversationLog);
 }
 
 // Function to display messages in the chat display
 function displayMessage(sender, message) {
     const chatDisplay = document.getElementById("chat-display");
     const messageElement = document.createElement("div");
+
+    // Replace "chat-message" with your actual CSS class for chat messages
     messageElement.className = "chat-message";
     messageElement.textContent = sender + message;
+
     chatDisplay.appendChild(messageElement);
 }
 
@@ -81,19 +94,17 @@ submitBtn.addEventListener("click", function() {
     displayMessage("User: ", userInput);
     conversationLog.push({ sender: "user", message: userInput });
     processUserInput(userInput);
-    document.getElementById("userInput").value = ""; // Clear user input
+
+    // Clear user input field
+    document.getElementById("userInput").value = "";
 });
 
-// Function to log user interactions
-function logInteraction(userInput, botResponse) {
-    const timestamp = new Date().toISOString();
-    const logEntry = `${timestamp} - User: ${userInput} | Bot: ${botResponse}\n`;
-    fs.appendFile('log.txt', logEntry, (err) => {
-        if (err) {
-            console.error('Error writing to log.txt:', err);
-        }
-    });
-}
+// Event listener for feedback
+const feedbackBtn = document.getElementById("feedbackBtn");
+feedbackBtn.addEventListener("click", function() {
+    const feedback = window.confirm("Were you happy with the response?");
+    handleFeedback(feedback);
+});
 
 // Initial bot greeting
 displayMessage("QP GPT: ", "Hello! I am QP GPT. How can I assist you today?");
