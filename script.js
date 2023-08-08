fa30db89fed5956cbb737ece2eddfa30265628e2
Qomplainerz <messanyrecordings@yahoo.de>
2023-08-09 00:07:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Ensure that the script has write permissions in the directory where the log.txt file is located
-
 const fs = require('fs');
+const readline = require('readline');
 
 // Function to load responses from responses.txt
 async function loadResponses() {
@@ -35,10 +34,8 @@ async function processUserInput(userInput) {
     const trimmedInput = userInput.trim().toLowerCase();
     let botResponse = "I am not sure how to respond to that";
 
-    // Load responses from responses.txt
     const responses = await loadResponses();
 
-    // Check if input matches any response
     for (const entry of responses) {
         if (trimmedInput.includes(entry.input)) {
             botResponse = entry.response;
@@ -47,25 +44,16 @@ async function processUserInput(userInput) {
     }
 
     if (trimmedInput.includes("tell me a joke")) {
-        // Load jokes from jokes.txt
         const jokes = await loadJokes();
-
-        // Randomly select a joke
         const randomIndex = Math.floor(Math.random() * jokes.length);
         botResponse = jokes[randomIndex];
     }
 
-    // Display bot's response
+    displayMessage("User: ", userInput);
     displayMessage("QP GPT: ", botResponse);
-
-    // Ask user for feedback
-    displayMessage("QP GPT: ", "Were you happy with the response?");
-
-    // Log user input, bot response, and feedback
-    logInteraction(userInput, botResponse); // <-- Add this line
+    logInteraction("User: " + userInput, "Bot: " + botResponse);
 }
 
-// Function to display messages in the chat display
 function displayMessage(sender, message) {
     const chatDisplay = document.getElementById("chat-display");
     const messageElement = document.createElement("div");
@@ -74,20 +62,17 @@ function displayMessage(sender, message) {
     chatDisplay.appendChild(messageElement);
 }
 
-// Event listener for user input
 const submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click", function() {
     const userInput = document.getElementById("userInput").value;
-    displayMessage("User: ", userInput);
-    conversationLog.push({ sender: "user", message: userInput });
     processUserInput(userInput);
+    logInteraction("User: " + userInput, ""); // Log user input
     document.getElementById("userInput").value = ""; // Clear user input
 });
 
-// Function to log user interactions
 function logInteraction(userInput, botResponse) {
     const timestamp = new Date().toISOString();
-    const logEntry = `${timestamp} - User: ${userInput} | Bot: ${botResponse}\n`;
+    const logEntry = `${timestamp} - ${userInput} | ${botResponse}\n`;
     fs.appendFile('log.txt', logEntry, (err) => {
         if (err) {
             console.error('Error writing to log.txt:', err);
@@ -95,5 +80,4 @@ function logInteraction(userInput, botResponse) {
     });
 }
 
-// Initial bot greeting
 displayMessage("QP GPT: ", "Hello! I am QP GPT. How can I assist you today?");
