91e9227f74faba521a8ca053894312344d779818
Qomplainerz <messanyrecordings@yahoo.de>
2023-08-09 00:13:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
+// Ensure that the script has write permissions in the directory where the log.txt file is located
+
+const fs = require('fs');
+
 // Function to load responses from responses.txt
 async function loadResponses() {
-    try {
-        const response = await fetch('responses.txt');
-        const data = await response.text();
-        return JSON.parse(data);
-    } catch (error) {
-        console.error('Error loading responses:', error);
-        return [];
-    }
+    return new Promise((resolve, reject) => {
+        fs.readFile('responses.txt', 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                const responses = JSON.parse(data);
+                resolve(responses);
+            }
+        });
+    });
 }
 
 // Function to load jokes from jokes.txt
 async function loadJokes() {
-    try {
-        const response = await fetch('jokes.txt');
-        const data = await response.text();
-        return JSON.parse(data);
-    } catch (error) {
-        console.error('Error loading jokes:', error);
-        return [];
-    }
+    return new Promise((resolve, reject) => {
+        fs.readFile('jokes.txt', 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                const jokes = JSON.parse(data);
+                resolve(jokes);
+            }
+        });
+    });
 }
 
 // Function to process user input
@@ -27,8 +35,10 @@ async function processUserInput(userInput) {
     const trimmedInput = userInput.trim().toLowerCase();
     let botResponse = "I am not sure how to respond to that";
 
+    // Load responses from responses.txt
     const responses = await loadResponses();
 
+    // Check if input matches any response
     for (const entry of responses) {
         if (trimmedInput.includes(entry.input)) {
             botResponse = entry.response;
@@ -37,16 +47,25 @@ async function processUserInput(userInput) {
     }
 
     if (trimmedInput.includes("tell me a joke")) {
+        // Load jokes from jokes.txt
         const jokes = await loadJokes();
+
+        // Randomly select a joke
         const randomIndex = Math.floor(Math.random() * jokes.length);
         botResponse = jokes[randomIndex];
     }
 
-    displayMessage("User: ", userInput);
+    // Display bot's response
     displayMessage("QP GPT: ", botResponse);
-    logInteraction("User: " + userInput, "Bot: " + botResponse);
+
+    // Ask user for feedback
+    displayMessage("QP GPT: ", "Were you happy with the response?");
+
+    // Log user input, bot response, and feedback
+    logInteraction(userInput, botResponse);
 }
 
+// Function to display messages in the chat display
 function displayMessage(sender, message) {
     const chatDisplay = document.getElementById("chat-display");
     const messageElement = document.createElement("div");
@@ -55,17 +74,20 @@ function displayMessage(sender, message) {
     chatDisplay.appendChild(messageElement);
 }
 
+// Event listener for user input
 const submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click", function() {
     const userInput = document.getElementById("userInput").value;
+    displayMessage("User: ", userInput);
+    conversationLog.push({ sender: "user", message: userInput });
     processUserInput(userInput);
-    logInteraction("User: " + userInput, ""); // Log user input
     document.getElementById("userInput").value = ""; // Clear user input
 });
 
+// Function to log user interactions
 function logInteraction(userInput, botResponse) {
     const timestamp = new Date().toISOString();
-    const logEntry = `${timestamp} - ${userInput} | ${botResponse}\n`;
+    const logEntry = `${timestamp} - User: ${userInput} | Bot: ${botResponse}\n`;
     fs.appendFile('log.txt', logEntry, (err) => {
         if (err) {
             console.error('Error writing to log.txt:', err);
@@ -73,4 +95,5 @@ function logInteraction(userInput, botResponse) {
     });
 }
 
+// Initial bot greeting
 displayMessage("QP GPT: ", "Hello! I am QP GPT. How can I assist you today?");
